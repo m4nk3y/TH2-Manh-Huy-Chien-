76e82d41dccb46ec6c67610e98179a1daaceca86
chientran2311 <146794919+chientran2311@users.noreply.github.com>
2024-12-05 14:44:19 +0700

    commit

--- views/home/index.php
+++ views/home/index.php
@@ -1,152 +1,150 @@
 <!doctype html>
 <html lang="en">
-    <head>
-        <title>Title</title>
-        <!-- Required meta tags -->
-        <meta charset="utf-8" />
-        <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1, shrink-to-fit=no"
-        />
 
-        <!-- Bootstrap CSS v5.2.1 -->
-        <link
-            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
-            rel="stylesheet"
-            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
-            crossorigin="anonymous"
-        />
-        <link rel="stylesheet" href="../../assets/css/header.css">
-    </head>
+<head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
-    <body>
+    <!-- Bootstrap CSS v5.2.1 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+    <link rel="stylesheet" href="../../assets/css/header.css">
+</head>
+
+<body>
+    <header>
         <header>
-        <header>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light ">
-            <div class="container">
-                <a class="navbar-brand " href=""><img src="../../assets/images/logo.png" class="img-fluid rounded-top" alt="" />
-                </a>
-                <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false"
-                    aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="collapsibleNavId">
-                    <ul class="navbar-nav me-auto mt-2 mt-lg-0">
-                        <li class="nav-item">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light ">
+                <div class="container">
+                    <a class="navbar-brand " href=""><img src="../../assets/images/logo.png"
+                            class="img-fluid rounded-top" alt="" />
+                    </a>
+                    <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false"
+                        aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="collapsibleNavId">
+                        <ul class="navbar-nav me-auto mt-2 mt-lg-0">
+                            <li class="nav-item">
 
-                        </li>
-                    </ul>
-                    <div class="d-flex flex-column ">
-                        <button type="button" class="btn btn-primary mb-2">
-                            Đăng nhập
+                            </li>
+                        </ul>
+                        <div class="d-flex flex-column ">
+                            <a name="" id="" class="btn btn-primary mb-2" href="index.php?controller=admin&action=" role="button">Button</a>
+
+                            <form class="d-flex my-2 my-lg-0">
+                                <input class="form-control me-sm-2" type="text" placeholder="Tìm kiếm" />
+                                <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">
+                                    Tìm
+                                </button>
+                            </form>
+                        </div>
+
+                    </div>
+                </div>
+            </nav>
+            <div class="backg">
+                <div class="container-lg mt-5">
+                    <div id="carouselExampleIndicators" class="carousel slide">
+                        <div class="carousel-indicators">
+                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0"
+                                class="active" aria-current="true" aria-label="Slide 1"></button>
+                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
+                                aria-label="Slide 2"></button>
+                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
+                                aria-label="Slide 3"></button>
+                        </div>
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img src="../../assets/images/baner-webthumb.jpg" class="d-block w-100" alt="...">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="../../assets/images/web1.jpg" class="d-block w-100" alt="...">
+                            </div>
+                        </div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
+                            data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
                         </button>
-                        <form class="d-flex my-2 my-lg-0">
-                            <input class="form-control me-sm-2" type="text" placeholder="Tìm kiếm" />
-                            <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">
-                                Tìm
-                            </button>
-                        </form>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
+                            data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
                     </div>
-
                 </div>
             </div>
-        </nav>
-        <div class="backg">
-        <div class="container-lg mt-5">
-        <div id="carouselExampleIndicators" class="carousel slide">
-            <div class="carousel-indicators">
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-            </div>
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <img src="../../assets/images/baner-webthumb.jpg" class="d-block w-100" alt="...">
-                </div>
-                <div class="carousel-item">
-                    <img src="../../assets/images/web1.jpg" class="d-block w-100" alt="...">
-                </div>
-            </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-            </button>
-        </div>
-        </div>
-        </div>
-    </header>
         </header>
-        <main></main>
-        <footer>
+    </header>
+    <main></main>
+    <footer>
         <!doctype html>
-<html lang="en">
-    <head>
-        <title>Title</title>
-        <!-- Required meta tags -->
-        <meta charset="utf-8" />
-        <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1, shrink-to-fit=no"
-        />
+        <html lang="en">
 
-        <!-- Bootstrap CSS v5.2.1 -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-        <link rel="stylesheet" href="../Css/domnet.css">
-    </head>
+        <head>
+            <title>Title</title>
+            <!-- Required meta tags -->
+            <meta charset="utf-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
-    <body>
-    
+            <!-- Bootstrap CSS v5.2.1 -->
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+                integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+                crossorigin="anonymous">
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+                integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+                crossorigin="anonymous"></script>
+            <link rel="stylesheet" href="../Css/domnet.css">
+        </head>
 
-   
-    <footer class="mt-auto text-white text-center ">
-        <div class="container-lg ">
-            
-        </div>
+        <body>
 
-        
-        <div class="row text-white" id="fut1">
-            <div class="col-3"></div>
-            <div class="col-6 text-center">
-                <h4 class="card-title my-4 text-center">@2024 TRƯỜNG ĐẠI HỌC THỦY LỢI</h4>
-            </div>
-            <div class="col-3"></div>
-        </div>
-        <div class="row text-white" id="fut">
-            <div class="col-3"></div>
-            <div class="col-3">
-            <img class="card-img-top mt-2 mx-2 mb-2" src="../../assets/images/robot.png" alt="Logo" />
-            </div>
-            <div class="col-3">
-                <div class="content d-flex my-3">
-                    
-                    <div class="card-body my-2 mx-3">
-                        <h4>TRƯỜNG ĐẠI HỌC THỦY LỢI</h4>
-                        <p class="h5">SDT: 0942131233</p>
-                        <p class="h5">Địa chỉ: 178 Tây Sơn</p>
-                        <p class="h5">Email: Skibidi@gmail.com</p>
-                    </div>
+
+
+            <footer class="mt-auto text-white text-center ">
+                <div class="container-lg ">
+
                 </div>
-            </div>
-            <div class="col-3"></div>
-        </div>
-    </footer>
-        
-        <script
-            src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
-            integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
-            crossorigin="anonymous"
-        ></script>
 
-        <script
-            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
-            integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
-            crossorigin="anonymous"
-        ></script>
-    </body>
-</html>
+
+                <div class="row text-white" id="fut1">
+                    <div class="col-3"></div>
+                    <div class="col-6 text-center">
+                        <h4 class="card-title my-4 text-center">@2024 TRƯỜNG ĐẠI HỌC THỦY LỢI</h4>
+                    </div>
+                    <div class="col-3"></div>
+                </div>
+                <div class="row text-white" id="fut">
+                    <div class="col-3"></div>
+                    <div class="col-3">
+                        <img class="card-img-top mt-2 mx-2 mb-2" src="../../assets/images/robot.png" alt="Logo" />
+                    </div>
+                    <div class="col-3">
+                        <div class="content d-flex my-3">
+
+                            <div class="card-body my-2 mx-3">
+                                <h4>TRƯỜNG ĐẠI HỌC THỦY LỢI</h4>
+                                <p class="h5">SDT: 0942131233</p>
+                                <p class="h5">Địa chỉ: 178 Tây Sơn</p>
+                                <p class="h5">Email: Skibidi@gmail.com</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-3"></div>
+                </div>
+            </footer>
+
+            <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+                integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+                crossorigin="anonymous"></script>
+
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+                integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+                crossorigin="anonymous"></script>
+        </body>
+
+        </html>
